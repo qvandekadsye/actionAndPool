1a46d09b74b06bde5b782107725c40a698c50330
Tanghe Jerome <tanghe@a16p2.fil.univ-lille1.fr>
2014-10-08 11:06:53 +0200

    Added new package for exceptions

---
+++ src/vandekadsye/tanghe/ActionAndPool/Exceptions/ActionFinishedException.java
@@ -0,0 +1,9 @@
+package vandekadsye.tanghe.ActionAndPool.Exceptions;
+
+public class ActionFinishedException extends Exception {
+	/**
+	 * Bien définir l'exception à hériter pour spécialiser
+	 * Runtime Exception, le programmeur ne doit pas les catcher, il n'est pas imposer de le faire.
+	 */
+
+}
