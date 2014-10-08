80ffd67987490b82fd829642e770aa409b557fe0
Van De Kadsye Quentin <vandekadsye@a16p1.fil.univ-lille1.fr>
2014-10-08 10:39:16 +0200

    Starting project by creating package

---
+++ src/vandekadsye/tanghe/ActionAndPool/Main.java
@@ -0,0 +1,13 @@
+package vandekadsye.tanghe.ActionAndPool;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
