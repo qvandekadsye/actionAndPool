3f70eb8b77133d5cd811c3f1cf73438a6a0434f9
Van De Kadsye Quentin <vandekadsye@a16p1.fil.univ-lille1.fr>
2014-10-08 11:03:18 +0200

    adding foreeableAction class and ActionFinishedException class

---
+++ src/vandekadsye/tanghe/ActionAndPool/ForseeableAction.java
@@ -0,0 +1,34 @@
+
+public class ForseeableAction extends Action{
+
+	private int currentState;
+	private int numberOfState;
+	
+	public ForseeableAction(int numberOfState)
+	{
+		this.numberOfState=numberOfState;
+		this.currentState=0;
+	}
+	
+	public boolean isFinished()
+	{
+		boolean result=false;
+		if(this.currentState==this.numberOfState)
+			result=true;
+		return result;
+	}
+	
+	public boolean isReady()
+	{
+		boolean result=false;
+		if(this.currentState==0)
+			result=true;
+		return result;
+		
+	}
+	
+	public void doStep()
+	{
+		this.currentState++;
+	}
+}
