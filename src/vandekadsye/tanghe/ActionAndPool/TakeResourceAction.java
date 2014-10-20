2eff37f063220f7040bbf331988da8d08d469283
Quentin <quentin@debian-quentin>
2014-10-20 13:21:04 +0200

    create takeressourceaction

--- src/vandekadsye/tanghe/ActionAndPool/TakeResourceAction.java
+++ src/vandekadsye/tanghe/ActionAndPool/TakeResourceAction.java
@@ -1,5 +1,43 @@
 package vandekadsye.tanghe.ActionAndPool;
 
-public class TakeResourceAction {
+import vandekadsye.tanghe.ActionAndPool.Exceptions.ActionFinishedException;
+
+public class TakeResourceAction extends Action {
+	private ResourcePool pool;
+	private ResourcefulUser user;
+	
+	/**
+	 * @param poolToUse The pool of resource to use.
+	 * @param aUser The user who will store the resource.
+	 */
+	public TakeResourceAction(ResourcePool poolToUse, ResourcefulUser aUser)
+	{
+		this.pool=poolToUse;
+		this.user = aUser;
+	}
+
+	@Override
+	public boolean isReady() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isInProgress() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean isFinished() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void doStep() throws ActionFinishedException {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
