59ada32eecc4f3e3644a283ca33ea34a0b87791f
Quentin <quentin@debian-quentin>
2014-10-21 18:02:29 +0200

    xorrecting an error in cubiclepool

--- src/vandekadsye/tanghe/ActionAndPool/CubiclePool.java
+++ src/vandekadsye/tanghe/ActionAndPool/CubiclePool.java
@@ -1,5 +1,16 @@
 package vandekadsye.tanghe.ActionAndPool;
 
-public class CubiclePool {
+public class CubiclePool extends ResourcePool<Cubicle> {
+	
+	public CubiclePool(int nbCubicle)
+	{
+		super(nbCubicle);
+	}
+
+	@Override
+	public Cubicle createInstanceOfR() {
+		// TODO Auto-generated method stub
+		return new Cubicle();
+	}
 
 }
