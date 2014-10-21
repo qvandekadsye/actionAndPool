f2d537aaf95bbfc131f2a7b3e0e7278ff1e5824b
Quentin <quentin@debian-quentin>
2014-10-21 13:19:53 +0200

    create swimmer class and modifiying todo

---
+++ src/vandekadsye/tanghe/ActionAndPool/Swimmer.java
@@ -0,0 +1,13 @@
+package vandekadsye.tanghe.ActionAndPool;
+
+public class Swimmer extends SequentialScheduler {
+	private String name;
+	private BasketPool basketManager;
+	private CubiclePool cubicleManager;
+	private int undressingTime, swimmingTime, dressingTime;
+	
+
+	
+	}
+
+
