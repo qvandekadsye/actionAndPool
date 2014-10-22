cdee902abe74fe04fdb2462db5e4febe4e87b088
Jérôme Tanghe <tanghe.jerome1@gmail.com>
2014-10-22 11:40:51 +0200

    Modified some classes

--- src/vandekadsye/tanghe/ActionAndPool/JUnit/SequentialSchedulerTest.java
+++ src/vandekadsye/tanghe/ActionAndPool/JUnit/SequentialSchedulerTest.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import org.junit.Test;
 
+import vandekadsye.tanghe.ActionAndPool.Action;
 import vandekadsye.tanghe.ActionAndPool.ForseeableAction;
 import vandekadsye.tanghe.ActionAndPool.SequentialScheduler;
 import vandekadsye.tanghe.ActionAndPool.Exceptions.ActionFinishedException;
@@ -15,9 +16,11 @@ public class SequentialSchedulerTest {
 
 	@Test
 	public void testProgression() {
-		List<ForseeableAction> list = new ArrayList<ForseeableAction>();
-		list.add(new ForseeableAction(1));
-		list.add(new ForseeableAction(2));
+		List<Action> list = new ArrayList<Action>();
+		Action a1 = new ForseeableAction(1);
+		Action a2 = new ForseeableAction(2);
+		list.add(a1);
+		list.add(a2);
 		
 		SequentialScheduler s = new SequentialScheduler(list);
 		/*
@@ -38,13 +41,13 @@ public class SequentialSchedulerTest {
 		assertFalse(s.isInProgress());
 		assertFalse(s.isFinished());
 		
-		assertTrue(s.getAction(0).isReady());
-		assertFalse(s.getAction(0).isInProgress());
-		assertFalse(s.getAction(0).isFinished());
+		assertTrue(a1.isReady());
+		assertFalse(a1.isInProgress());
+		assertFalse(a1.isFinished());
 		
-		assertTrue(s.getAction(1).isReady());
-		assertFalse(s.getAction(1).isInProgress());
-		assertFalse(s.getAction(1).isFinished());
+		assertTrue(a2.isReady());
+		assertFalse(a2.isInProgress());
+		assertFalse(a2.isFinished());
 		
 		// Doing a step
 		try {
@@ -52,6 +55,7 @@ public class SequentialSchedulerTest {
 		} catch (ActionFinishedException e) {
 			fail("Thrown ActionFinishedException at a time it should not.");
 		}
+		
 		
 		/*
 		 * At this time:
@@ -71,13 +75,13 @@ public class SequentialSchedulerTest {
 		assertTrue(s.isInProgress());
 		assertFalse(s.isFinished());
 		
-		assertFalse(s.getAction(0).isReady());
-		assertFalse(s.getAction(0).isInProgress());
-		assertTrue(s.getAction(0).isFinished());
+		assertFalse(a1.isReady());
+		assertFalse(a1.isInProgress());
+		assertTrue(a1.isFinished());
 		
-		assertTrue(s.getAction(1).isReady());
-		assertFalse(s.getAction(1).isInProgress());
-		assertFalse(s.getAction(1).isFinished());
+		assertTrue(a2.isReady());
+		assertFalse(a2.isInProgress());
+		assertFalse(a2.isFinished());
 		
 		// Doing a step
 		try {
@@ -104,13 +108,13 @@ public class SequentialSchedulerTest {
 		assertTrue(s.isInProgress());
 		assertFalse(s.isFinished());
 		
-		assertFalse(s.getAction(0).isReady());
-		assertFalse(s.getAction(0).isInProgress());
-		assertTrue(s.getAction(0).isFinished());
+		assertFalse(a1.isReady());
+		assertFalse(a1.isInProgress());
+		assertTrue(a1.isFinished());
 		
-		assertFalse(s.getAction(1).isReady());
-		assertTrue(s.getAction(1).isInProgress());
-		assertFalse(s.getAction(1).isFinished());
+		assertFalse(a2.isReady());
+		assertTrue(a2.isInProgress());
+		assertFalse(a2.isFinished());
 		
 		// Doing a step
 		try {
--- src/vandekadsye/tanghe/ActionAndPool/Scheduler.java
+++ src/vandekadsye/tanghe/ActionAndPool/Scheduler.java
@@ -7,14 +7,14 @@ import java.util.List;
  * Manages a list of actions.
  * @param <A> the type of action
  */
-public abstract class Scheduler<A extends Action> extends Action {
+public abstract class Scheduler extends Action {
 
-    protected List<A> actions;
+    protected List<Action> actions;
     protected int progress;
     
     public Scheduler() {
     
-        actions = new ArrayList<A>();
+        actions = new ArrayList<Action>();
         progress = 0;
     
     }
@@ -22,7 +22,7 @@ public abstract class Scheduler<A extends Action> extends Action {
     /**
      * @param actions2 a list of actions. Must be of type <code>List&lt;A&gt;</code> where A extends <code>Action</code>
      */
-    public Scheduler(List<A> actions2) {
+    public Scheduler(List<Action> actions2) {
     
         this.actions = actions2;
         progress = 0;
@@ -32,13 +32,13 @@ public abstract class Scheduler<A extends Action> extends Action {
     /**
      * @return a list of actions to be processed
      */
-    public List<A> getActions() {
+    public List<Action> getActions() {
     
         return actions;
     
     }
     
-    public A getAction(int index) {
+    public Action getAction(int index) {
     	return getActions().get(index);
     }
     
@@ -46,7 +46,7 @@ public abstract class Scheduler<A extends Action> extends Action {
      * Adds <code>a</code> to the list of actions to process.
      * @param a the action to add to the list.
      */
-    public void addAction(A a) {
+    public void addAction(Action a) {
     
         actions.add(a);
     
