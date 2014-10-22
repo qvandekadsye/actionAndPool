a064ce83df7def9aaa33f30b1d8eca837b93bc03
Van De Kadsye Quentin <vandekadsye@a10p2.fil.univ-lille1.fr>
2014-10-22 09:25:12 +0200

    Modifiying Swimmer class

--- src/vandekadsye/tanghe/ActionAndPool/Swimmer.java
+++ src/vandekadsye/tanghe/ActionAndPool/Swimmer.java
@@ -6,6 +6,35 @@ public class Swimmer extends SequentialScheduler {
 	private CubiclePool cubicleManager;
 	private int undressingTime, swimmingTime, dressingTime;
 	
+	/**
+	 * Swimmer class constructor.
+	 * @param swimmerName Name of the swimmer.
+	 * @param basketPoolToUse The basket provider.
+	 * @param cubiclePoolToUse The cubicle provider.
+	 * @param timeToUndress Steps needed for the undressing.
+	 * @param timeToSwim  Steps needed for the swimming.
+	 * @param timeToDress Steps needed for the swimming.
+	 */
+	public Swimmer(String swimmerName, BasketPool basketPoolToUse, CubiclePool cubiclePoolToUse, int timeToUndress, int timeToSwim, int timeToDress)
+	{
+		this.name=swimmerName;
+		this.basketManager=basketPoolToUse;
+		this.cubicleManager=cubiclePoolToUse;
+		this.undressingTime=timeToUndress;
+		this.swimmingTime=timeToSwim;
+		this.dressingTime=timeToDress;
+		this.actions.addAction(new TakeResourceAction(basketPoolToUse,new ResourcefulUser()));
+		this.actions.addAction(new TakeResourceAction(cubiclePoolToUse,new ResourcefulUser()));
+		this.actions.addAction(new ForseeableAction(timeToUndress));
+		//this.actions.addActions(new FreeResourceAction());
+		this.actions.addAction(new ForseeableAction(timeToSwim));
+		this.actions.addAction(new TakeResourceAction(cubiclePoolToUse,new ResourcefulUser()));
+		this.actions.addAction(new ForseeableAction(timeToDress));
+		//this.actions.addActions(new FreeResourceAction());
+		//this.actions.addActions(new FreeResourceAction());
+		
+		
+	}
 
 	
 	}
