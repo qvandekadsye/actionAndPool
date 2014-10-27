5d26abbc90dbdd418176d46b95961d2608157f60
Jérôme Deuchnord <contact@deuchnord.tk>
2014-10-27 08:49:01 +0100

    Added JUnit test for ForseeableAction

---
+++ src/vandekadsye/tanghe/ActionAndPool/JUnit/ForseeableActionTest.java
@@ -0,0 +1,34 @@
+package vandekadsye.tanghe.ActionAndPool.JUnit;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ForseeableActionTest {
+
+	@Test
+	public void testIsReady() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsInProgress() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIsFinished() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testDoStep() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testForseeableAction() {
+		fail("Not yet implemented");
+	}
+
+}
